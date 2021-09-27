1736b0403107b1631f91cee87211782089127787
Neil Katin <github2@askneil.com>
2021-09-27 12:27:11 -0700

    periodic update

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ avis_downloads
 
 Package to remotely drive the avis website to download receipts and rental info.
 
+Installing:
+
+cygwin needs libffi-devel in order to install numpy (which is used by py-pdf-parser)
+
--- avisweb.py
+++ avisweb.py
@@ -6,11 +6,12 @@ import argparse
 import logging
 import re
 import datetime
+import time
 
 #from http.cookiejar import LWPCookieJar, Cookie
 
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -33,15 +34,19 @@ def main():
     config = neil_tools.init_config(config_static, ".env")
 
     # initialize selenium
-    driver = init_selenium.get_driver(headless=(not args.show))
+    try:
+        driver = init_selenium.get_driver(headless=(not args.show))
 
-    for dr in args.dr_id:
-        pass
+        for dr in args.dr_id:
+            pass
 
-        # should fetch DTT info here...
+            # should fetch DTT info here...
 
-        # now drive the avis site
-        fetch_receipt(config, driver, "mccrindle", "12140177US0")
+            # now drive the avis site
+            #fetch_receipt(config, driver, "mccrindle", "12140177US0")
+            fetch_receipt(config, driver, "Mattimoe", "12445864US4")
+    finally:
+        driver.close()
 
 
 def fetch_receipt(config, driver, name, reservation):
@@ -51,6 +56,43 @@ def fetch_receipt(config, driver, name, reservation):
     WebDriverWait(driver, 30).until(EC.title_contains(config.AVIS_RECEIPT_TITLE))
 
     log.debug(f"after page load")
+
+    time.sleep(0.5)
+
+    # there's a marketing popup that shows up and has to be dismissed
+    driver.find_element_by_tag_name("body").send_keys(Keys.ESCAPE)
+    time.sleep(0.5)
+
+    form = driver.find_element_by_name("receipt_form_hidden")
+    form.find_element_by_id("lastName").send_keys(name)
+    form.find_element_by_id("Confirmation-no").send_keys(reservation)
+    button = form.find_element_by_tag_name("button").click()
+    time.sleep(5)
+
+    # check the title; 
+    title = driver.title
+    if title.startswith("Reservations"):
+        pass
+    elif title.startswith("Receipt Details"):
+        download_receipt(config, driver)
+    else:
+        log.error(f"Could not find avis record for '{ name }' / '{ reservation }'")
+        return
+
+    time.sleep(15)
+
+
+
+def download_receipt(config, driver):
+
+
+    outer_dashboard = driver.find_element_by_class_name("dashboard-content-container")
+    download_button = outer_dashboard.find_element_by_css_selector('a[ng-click="vm.downloadPdf()"]')
+    download_button.click()
+
+    
+
+    
 
 
 
